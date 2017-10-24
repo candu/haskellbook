879cb05a9b8215820b8239b2022c42ab384f17b6
Evan Savage <evan@savageinter.net>
2017-10-24 13:17:10 -0400

    Chapter 4 exercises

---
+++ ch4/ch4.hs
@@ -0,0 +1,11 @@
+-- ch4.hs
+module Ch4 where
+
+isPalindrome :: (Eq a) => [a] -> Bool
+isPalindrome xs = (reverse xs) == xs
+
+myAbs :: Integer -> Integer
+myAbs x = if x >= 0 then x else (-x)
+
+f :: (a, b) -> (c, d) -> ((b, d), (a, c))
+f x y = ((snd x, snd y), (fst x, fst y))
